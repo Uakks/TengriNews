f726bd6035db7f282e5f01238bf48f73acbd9300
Uakks <sopygaliu@mail.ru>
2024-04-09 22:58:06 +0500

    Number of pages changed

--- Tengri/management/commands/parsing.py
+++ Tengri/management/commands/parsing.py
@@ -45,7 +45,7 @@ class Command(BaseCommand):
         abspath = os.path.abspath(os.path.join(cwd))
         os.mkdir(f"{abspath}/data")
 
-        iters = 2
+        iters = 11
 
         for i in range(1, iters):
             req = requests.get(url + f"page/{i}", headers)
